2266a6c0599fea808c8b5f637c7c6416936b5ba4
NARARIA03 <hyeonseong0305@gmail.com>
2025-09-13 17:05:11 +0900

    feat: 미들웨어에서 미로그인 / 접근 방지 (#5)

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,14 +2,20 @@ import { ACCESS_TOKEN_KEY } from '@/modules/axios/constants';
 import { type NextRequest, NextResponse } from 'next/server';
 
 export const config = {
-  matcher: ['/home/:path*'],
+  matcher: ['/', '/home/:path*'],
 };
 
 export function middleware(req: NextRequest) {
   const accessToken = req.cookies.get(ACCESS_TOKEN_KEY);
 
+  // ! 미로그인 검증
   if (!accessToken) {
     const fullPath = req.nextUrl.pathname + req.nextUrl.search;
     return NextResponse.redirect(new URL(`/login?nextUri=${encodeURIComponent(fullPath)}`, req.url));
+  }
+
+  // ! 로그인 상태로 / 접근 방지
+  if (req.nextUrl.pathname === '/') {
+    return NextResponse.redirect(new URL('/home', req.url));
   }
 }
